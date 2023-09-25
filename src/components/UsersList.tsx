8f0e168863e8cd93b3db78a4d183add308ceae05
marshall-bits <kz_@hotmail.es>
2023-09-25 18:24:14 +0200

    add styles in usersList

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,37 +1,46 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import styled from "styled-components";
 import { UsersContext } from "../context/UsersContext";
+import { Avatar } from "../components/Avatar";
 
 const ListContainer = styled.div`
     display: flex;
     flex-direction: column;
     list-style: none;
     list-style-type: none;
-    padding: 1.5rem 1rem;
+    padding: 1rem .5rem;
+    width: 100%;
+    max-width: 90vw;
     gap: 1em;
-    font-size: 1.5em;
+    font-size: 1.3em;
     max-height: 50vh;
     overflow-y: scroll;
     background-color: #07070733;
     border-radius: .5rem;
+    transition: all .3s ease-in-out;
 
     li{
         display: flex;
         justify-content: space-between;
         align-items: center;
         height: 7vh;
+        width: 100%;
     }
 
     button {
-        font-size: 13px;
+        font-size: .2rem;
         padding: 10px;
     }
 
-    img {
-        width: 2rem;
+    .delete {
+        width: 1.3rem;
+        cursor: pointer;
+        fill: #15c42c;
+        background-color: rgb(255, 255, 115);
+        border-radius: 50%;
     }
-`
 
+`
 
 export const UsersList = () => {
     const { usersState, dispatch } = useContext(UsersContext);
@@ -39,20 +48,15 @@ export const UsersList = () => {
 
     return (
         <ListContainer>
-            {users.map((user, index) => (
+            {users.slice().reverse().map((user, index) => (
                 <li key={index}>
-                    <img
-                        onClick={() => {
-                            dispatch({
-                                type: 'UPDATE_USER_AVATAR_URL',
-                                payload: user.id
-                            })
-                        }}
-                        src={user.avatar} alt="user avatar" />
+                    <Avatar user={user} />
                     <p>
                         {user.name}
                     </p>
-                    <button
+                    <img
+                        className="delete"
+                        src="/icons-trash.svg" alt="delete user"
                         onClick={
                             () => {
                                 dispatch({
@@ -60,7 +64,7 @@ export const UsersList = () => {
                                     payload: user.id
                                 })
                             }
-                        }>Borrar</button>
+                        } />
                 </li>
             ))}
         </ListContainer>
