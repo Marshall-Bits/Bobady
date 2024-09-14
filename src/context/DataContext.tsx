1f7dad2eb88c6e138b0bcb002263f2f521fe778f
Marshall-Bits <kz_@hotmail.es>
2024-09-14 17:51:33 +0200

    redifine datacontext props

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -4,7 +4,10 @@ import { IQuizElement } from "../interfaces/interfaces";
 export type DataContextProps = {
   questions: IQuizElement[];
   challenges: IQuizElement[];
-  setCategory: (category: string) => void;
+  // setCategory: (category: string) => void;
+  getData: (category: string) => void;
+  loading: boolean;
+  setLoading: (loading: boolean) => void;
 };
 
 export const DataContext = createContext<DataContextProps>(
